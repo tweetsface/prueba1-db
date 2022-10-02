b54abaef9441c65b6b0ead11ebdac4839de4311f
tweetsface <15171236@itculiacan.edu.mx>
2022-10-02 14:08:53 -0600

    Se crea la parte del backend para consumir la api de los tenis

--- app/Http/Controllers/ColorController.php
+++ app/Http/Controllers/ColorController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+use Response;
+
+
 
 class ColorController extends Controller
 {
-    //
+    public  function index()
+    {
+      $response = Http::get('https://my-json-server.typicode.com/dp-danielortiz/dptest_jsonplaceholder/items',[
+        'headers' => [
+            'User-Agent' => 'user/1.0',
+            'Accept'     => 'application/json',
+        ]
+        ])->object();
+        $response = $this->getResponse($response);
+        return  $response ; 
+    }
+
+    public function getResponse($array)
+    {
+      $obj = array();
+      foreach($array as  $colors){
+        array_push($obj,["color"=>$colors->color]); 
+      }
+      return $obj;
+    }
+
+
 }
